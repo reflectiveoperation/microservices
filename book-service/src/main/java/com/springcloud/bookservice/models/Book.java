2432db9fc959dc39658f2c93ac93845e74d9c7fa
Cristian Perez <cristian.perez2011@gmail.com>
2019-05-15 16:02:25 +0300

    First commit

---
+++ book-service/src/main/java/com/springcloud/bookservice/models/Book.java
@@ -0,0 +1,38 @@
+package com.springcloud.bookservice.models;
+
+public class Book {
+
+    private Long id;
+    private String author;
+    private String title;
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
